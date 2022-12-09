34432cfbbbbfe7a54fef75ba1686e584245673a5
Marchejd <115989035+Marchejd@users.noreply.github.com>
2022-12-09 15:48:02 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "lib.h"
+
+using namespace std;
 
 int main(){
+      int x=0, s=0;
+    cin>>x;
+    int ar[x];
+    for (int i=0; i<x; i++)
+    {
+        cin>>ar[i];
+        s=max(ar[i]);
+    }
+    cout<<s;
+
   return 0;
 }
