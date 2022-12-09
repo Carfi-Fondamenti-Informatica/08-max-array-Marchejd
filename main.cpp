b1d9e522967afd2f10cb1ba6c2ca4bc95c02c8ec
Marchejd <115989035+Marchejd@users.noreply.github.com>
2022-12-09 15:51:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,15 +4,19 @@
 using namespace std;
 
 int main(){
+      
       int x=0;
       float s=0;
+      
     cin>>x;
     float ar[x];
+      
     for (int i=0; i<x; i++)
     {
         cin>>ar[i];
         s=max(ar[i]);
     }
+      
     cout<<s;
 
   return 0;
